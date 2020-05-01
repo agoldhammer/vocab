7fd3ca8509cf507ffe5fc82cb20d63f2a823d8ca
Art Goldhammer <art.goldhammer@gmail.com>
2020-05-01 16:35:10 -0400

    linter to flake8

--- vocab/createdb.py
+++ vocab/createdb.py
@@ -1,12 +1,15 @@
 import sqlite3
 import sys
 
+
 def create_db(name):
     conn = sqlite3.connect(name + '.db')
     curs = conn.cursor()
-    curs.execute("""CREATE TABLE vocab 
-        (src text, target text, supp text, lrd_from integer, lrd_to integer)""")
+    curs.execute("""CREATE TABLE vocab
+        (src text, target text, supp text,
+        lrd_from integer, lrd_to integer)""")
     conn.close()
+
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -14,4 +17,3 @@ if __name__ == "__main__":
         sys.exit(1)
     else:
         create_db(sys.argv[1])
-
--- vocab/vocab.py
+++ vocab/vocab.py
@@ -1,35 +1,38 @@
 import docx
-import click
+# import click
 import sqlite3
 
 myfname = "/Users/agold/Google Drive/Vocabulary/German.docx"
 
+
 def get_doc(fname):
     return docx.Document(fname)
+
 
 def get_vitems(doc):
     return [p.text.split(':') for p in doc.paragraphs]
 
+
 def validate_vitem(vitem):
-    "return status, vitem where status is t/f and vitem is of len 3 with blank string added to supp field if nec"
-    l = len(vitem)
-    if l == 2:
+    """return status, vitem where status is t/f and vitem is of len 3 with blank
+    string added to supp field if nec"""
+    lvitem = len(vitem)
+    if lvitem == 2:
         vitem.append('')
         return True, vitem
-    elif l == 3:
+    elif lvitem == 3:
         return True, vitem
     else:
         return False, vitem
 
-""" def print_vitems(vitems):
-    for vitem in vitems:
-        print([text.strip() for text in vitem])
 
-def print_bad(vitems):
-    for vitem in vitems:
-        l = len(vitem)
-        if l != 2 and l != 3:
-            print(f'Bad vitem {vitem}') """
+def store_data(data):
+    conn = sqlite3.connect('german.db')
+    c = conn.cursor()
+    c.executemany("INSERT INTO vocab VALUES (?, ?, ?, ?, ?)", data)
+    conn.commit()
+    conn.close()
+
 
 if __name__ == "__main__":
     doc = get_doc(myfname)
@@ -40,11 +43,13 @@ if __name__ == "__main__":
         status, v = validate_vitem(vitem)
         print(status, v)
         if status:
-            valid_vitems.append([v[0].strip(), v[1].strip(), v[2].strip(), 0, 0])
+            valid_vitems.append([v[0].strip(), v[1].strip(),
+                                v[2].strip(), 0, 0])
         else:
             invalid_vitems.append(v)
     for vitem in valid_vitems:
         print(vitem)
+    store_data([tuple(vitem) for vitem in valid_vitems])
     for vitem in invalid_vitems:
         print(f'Bad vitem: {vitem}')
     print(f'Total vitems: {len(vitems)}')
