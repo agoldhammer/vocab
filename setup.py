3e4dc06d35e9cc55fee42a31938833cc25887ad1
Art Goldhammer <art.goldhammer@gmail.com>
2020-06-19 10:33:11 -0400

    added cli.py, slexy entry point

--- setup.py
+++ setup.py
@@ -3,4 +3,9 @@ setup(
     name="vocab",
     version="0.1",
     packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "slexy = vocab.cli:main"
+        ]
+    }
 )
