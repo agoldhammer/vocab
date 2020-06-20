8f3a163ffa347b9c57907f39fe0268665bed574e
Art Goldhammer <art.goldhammer@gmail.com>
2020-06-19 20:18:54 -0400

    refactored addvocab

--- setup.py
+++ setup.py
@@ -1,11 +1,8 @@
 from setuptools import setup, find_packages
+
 setup(
     name="vocab",
     version="0.1",
     packages=find_packages(),
-    entry_points={
-        "console_scripts": [
-            "slexy = vocab.cli:main"
-        ]
-    }
+    entry_points={"console_scripts": ["slexy = vocab.cli:main"]},
 )
