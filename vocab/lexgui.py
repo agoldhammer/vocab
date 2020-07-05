6927155a48de571ce06df116e36f9bca53ea6902
Art Goldhammer <art.goldhammer@gmail.com>
2020-07-05 16:39:48 -0400

    redid window_update

--- vocab/lexgui.py
+++ vocab/lexgui.py
@@ -1,4 +1,3 @@
-from dataclasses import dataclass
 from enum import Enum
 
 import PySimpleGUI as sg
@@ -11,17 +10,6 @@ SMALL_FONT = "Helvetica 11"
 sg.theme("LightBlue3")  # Add a touch of color
 
 
-@dataclass
-class UpdateVector:
-    word: str
-    defn: str
-    supp: str
-    nseen: int
-    show_btn: bool
-    right_btn: bool
-    wrong_btn: bool
-
-
 # state machine for display
 # states
 class STATES(Enum):
@@ -31,21 +19,38 @@ class STATES(Enum):
     WORD_DISPLAYED = 2
 
 
-def window_update(window, upd_vec):
-    window["-WRD-"].update(upd_vec.word)
-    window["-DEF-"].update(upd_vec.defn)
-    window["-SUP-"].update(upd_vec.supp)
-    window["-NSEEN-"].update(f"Times seen: {upd_vec.nseen}")
-    window["-SHOWDEF-"].update(disabled=upd_vec.show_btn)
-    window["Right"].update(disabled=upd_vec.right_btn)
-    window["Wrong"].update(disabled=upd_vec.wrong_btn)
+def window_update(window, vitem, forward, state):
+    word = vitem.src if forward else vitem.target
+    defn = vitem.target if forward else vitem.src
+    window["-WRD-"].update(word)
+    if state == STATES.NEW_WORD:
+        supp = ""
+        defn = ""
+        dis_def = False
+        dis_rw = True
+    elif state == STATES.WORD_DISPLAYED:
+        supp = ""
+        dis_def = True
+        dis_rw = False
+    elif state == STATES.DEF_SHOWING:
+        supp = vitem.supp
+        dis_def = True
+        dis_rw = False
+
+    window["-DEF-"].update(defn)
+    window["-SUP-"].update(supp)
+    window["-NSEEN-"].update(f"Times seen: {vitem.nseen}")
+
+    window["-SHOWDEF-"].update(disabled=dis_def)
+    window["Right"].update(disabled=dis_rw)
+    window["Wrong"].update(disabled=dis_rw)
 
 
-def init_window(vitem):
+def init_window(vitem, forward):
     # All the stuff inside your window.
-
+    word = vitem.src if forward else vitem.target
     layout = [
-        [sg.Text(vitem.src, font=FONT, key="-WRD-", size=(60, 1))],
+        [sg.Text(word, font=FONT, key="-WRD-", size=(60, 1))],
         [sg.Text(f"Times seen: {vitem.nseen}", font=SMALL_FONT, key="-NSEEN-", size=(20, 1))],
         [sg.Text("", font=FONT, size=(60, 1), key="-DEF-", auto_size_text=True)],
         [sg.Text("", font=FONT, size=(60, 3), key="-SUP-")],
@@ -59,32 +64,32 @@ def init_window(vitem):
     # Event Loop to process "events" and get the "values" of the inputs
 
 
-def run_gui(vitems):
+def run_gui(vitems, conn, forward):
     window = None
     state = STATES.INIT
     for vitem in vitems:
-        # print("src", vitem.src)
+        print("src", vitem.src)
         if state == STATES.INIT:
-            window = init_window(vitem)
+            window = init_window(vitem, forward)
             state = STATES.WORD_DISPLAYED
         else:
             if state == STATES.NEW_WORD:
                 # print("new word")
-                upd_vec = UpdateVector(vitem.src, "", "", vitem.nseen, False, True, True)
-                window_update(window, upd_vec)
+                # upd_vec = UpdateVector(vitem.src, "", "", vitem.nseen, False, True, True)
+                window_update(window, vitem, forward, state)
                 state = STATES.WORD_DISPLAYED
         event, values = window.read(timeout=500)  # use timeout version for debug btn
         # event, values = window.read()
-        # print(f"after read: s {state}, e {event}, v {values}")
+        print(f"after read: s {state}, e {event}, v {values}")
 
         while(state != STATES.NEW_WORD):
             if event == "-SHOWDEF-":
-                # print("show")
-                upd_vec = UpdateVector(vitem.src, vitem.target, vitem.supp, vitem.nseen, True, False, False)
-                window_update(window, upd_vec)
+                print("show")
+                # upd_vec = UpdateVector(vitem.src, vitem.target, vitem.supp, vitem.nseen, True, False, False)
+                window_update(window, vitem, forward, state)
                 state = STATES.DEF_SHOWING
             elif event == "Right" or event == "Wrong":
-                # print("r/w")
+                print("r/w")
                 state = STATES.NEW_WORD
                 break
             elif event == "DEBUG":
@@ -98,4 +103,4 @@ def run_gui(vitems):
 
 def gui_conn(n, conn, forward, unlearned):
     vitems = gather_selected(n, conn, forward, unlearned)
-    run_gui(vitems)
+    run_gui(vitems, conn, forward)
