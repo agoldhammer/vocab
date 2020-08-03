f3202ae37c3cd25a454f10d1e4e91011a6286f47
Art Goldhammer <art.goldhammer@gmail.com>
2020-08-03 14:25:48 -0400

    working with proxy setup

--- vocab/server.py
+++ vocab/server.py
@@ -1,6 +1,4 @@
-from flask import (Flask, jsonify, session,
-                   render_template, request,
-                   send_from_directory)
+from flask import (Flask, session, request)
 
 from vocab.fileman import get_all_dbs, db_connect
 from vocab.practice import get_count, fetch_nitems
@@ -17,50 +15,16 @@ app = Flask(__name__,
 app.secret_key = b'96\x91Q\xf1N\x86\x1b\xc3&1\x92\x9f\tU\xca'
 
 
-@app.route("/")
-def index():
-    return(render_template("index.html"))
-
-
-@app.route("/css/<fname>")
-def css(fname):
-    print(fname)
-    return send_from_directory(f"{site_path}/css/", fname)
-
-
-@app.route("/js/compiled/<fname>")
-def js(fname):
-    print(fname)
-    return send_from_directory(f"{site_path}/js/compiled", fname)
-
-
-# TODO: for dev only, remove later
-def prepare_response(resp, status_code):
-    """add headers to response
-
-    Args:
-        resp ([raw response]): [response to request]
-
-    Returns:
-        [json resp]: [with heaers]
-    """
-    response = jsonify(resp)
-    response.headers.set('Access-Control-Allow-Origin', 'http://localhost:8700')
-    response.headers.set('Access-Control-Allow-Methods', 'GET, POST')
-    response.headers.set('Access-Control-Allow-Credentials', "true")
-    return response, status_code
-
-
 @app.route("/files/<pattern>")
 def get_files(pattern):
     files = get_all_dbs(pattern)
-    return prepare_response(files, 200)
+    return {"files": files}
 
 
 @app.route("/seldb/<dbname>")
 def sel_db(dbname):
     session["active_db"] = dbname
-    return prepare_response("active db set", 200)
+    return {"ok": "active db set"}
 
 
 def get_conn():
@@ -82,9 +46,9 @@ def getcount():
         resp = {"total": total,
                 "nfrom": nfrom,
                 "nto": nto}
-        return prepare_response(resp, 200)
+        return resp
     except ServerException as e:
-        return prepare_response(f"internal error: {e}")
+        return f"Internal error {e}", 500
 
 
 @app.route("/fetch")
@@ -92,12 +56,12 @@ def fetch():
     try:
         conn = get_conn()
         curs = conn.cursor()
-        slugs = fetch_nitems(curs, 5, True, False, web=True)
+        slugs = fetch_nitems(curs, 50, True, False, web=True)
         conn.close()
         resp = {"slugs": slugs,
                 "count": len(slugs),
                 "dir": "fwd",
                 "unlearned": False}
-        return prepare_response(resp, 200)
+        return resp
     except ServerException as e:
-        return f"Internal error: {e}"
+        return f"Internal error: {e}", 500
