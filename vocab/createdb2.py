18d373f1894748520d928849f105bb3d97b4abd9
Art Goldhammer <art.goldhammer@gmail.com>
2020-08-07 08:48:16 -0400

    added create2

---
+++ vocab/createdb2.py
@@ -0,0 +1,37 @@
+import sys
+
+from vocab.fileman import db_connect
+
+
+def create_db(name):
+    """create db for use by slexy
+    fields: src text, target text, supplementary text
+
+    Args:
+        name (str): name of db to create, w/o extension
+    """
+    conn = db_connect(name, create=True)
+    curs = conn.cursor()
+    curs.execute(
+        """CREATE TABLE vocab
+        (src text, target text, supp text)"""
+    )
+    curs.execute(
+        """
+        CREATE TABLE users
+        (uname text, upw text)
+        """
+    )
+    curs.execute(
+        "INSERT INTO users VALUES ('test', 'dummy')"
+    )
+    conn.commit()
+    conn.close()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Must specify db name")
+        sys.exit(1)
+    else:
+        create_db(sys.argv[1])
