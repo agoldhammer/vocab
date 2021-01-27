c642346596eb69467c4969d5554a9e6efff9b2d7
Art Goldhammer <art.goldhammer@gmail.com>
2021-01-26 21:21:20 -0500

    modified server-setup

--- pc-server-setup.sh
+++ pc-server-setup.sh
@@ -1,6 +1,6 @@
 export FLASK_APP=~/Prog/vocab/vocab/server.py
 export FLASK_ENV=development
 export SETTINGS=~/Prog/vocab/vocab/pc-server-settings.ini
-export LANG=redux2
+export LANG=redux3
 flask run
 
