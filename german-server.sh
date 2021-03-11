846c6240f062eda5c5989872f7bd8d79b181a288
Art Goldhammer <art.goldhammer@gmail.com>
2021-03-11 16:07:57 -0500

    added shell scripts for ger, ital

---
+++ german-server.sh
@@ -0,0 +1,6 @@
+export FLASK_APP=~/Prog/vocab/vocab/server.py
+export FLASK_ENV=development
+export SETTINGS=~/Prog/vocab/vocab/pc-server-settings.ini
+export LANG=german-new
+flask run -p 5000
+
---
+++ italian-server.sh
@@ -0,0 +1,7 @@
+export FLASK_APP=~/Prog/vocab/vocab/server.py
+export FLASK_ENV=development
+export SETTINGS=~/Prog/vocab/vocab/pc-server-settings.ini
+export LANG=italian-new
+flask run -p 5001
+
+
